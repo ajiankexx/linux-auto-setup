9d970fc3e4aec14a62dcf6969a926aa403914478
root <root@LAPTOP-JG03OJAB.>
2024-07-08 16:51:07 +0800

    init

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt update && sudo apt upgrade -y
+#install nvim
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
+mv squashfs-root /
+ln -s /squashfs-root/AppRun /usr/bin/nvim
